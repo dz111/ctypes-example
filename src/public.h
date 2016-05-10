25e85c61906257d1709e86201233b704c4de8b59
David Zhong <dzhong111@gmail.com>
2016-05-10 15:16:30 +1000

    Added dllexport

--- src/public.h
+++ src/public.h
@@ -3,17 +3,27 @@
 
 class StudentImpl;
 
+#if _WIN32
+#define STUDENT_API __declspec(dllexport)
+#else  // !_WIN32
+#define STUDENT_API
+#endif  // _WIN32
+
+#ifdef __cplusplus
 extern "C" {
+#endif  // __cplusplus
 
 typedef StudentImpl* Student;
 // typedef void* Student;
 
-Student student_create(const char* name);
+STUDENT_API Student student_create(const char* name);
 
-const char* student_name(Student student);
+STUDENT_API const char* student_name(Student student);
 
-void student_delete(Student student);
+STUDENT_API void student_delete(Student student);
 
+#ifdef __cplusplus
 };  // extern "C"
+#endif  // __cplusplus
 
 #endif  // _public_h_
